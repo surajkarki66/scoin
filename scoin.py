436837313e71e3d5fe1bb464afcc27079c500c1d
surajkarki66 <suraj.karki500@protonmail.com>
2021-10-25 21:42:54 +0545

    Created add_transaction api endpoint

--- scoin.py
+++ scoin.py
@@ -1,4 +1,5 @@
 from flask import jsonify
+from uuid import uuid4
 
 import config
 from app import create_app
@@ -15,18 +16,19 @@ def mine_block():
     previous_proof = previous_block['proof']
     proof = blockchain.proof_of_work(previous_proof)
     previous_hash = blockchain.hash(previous_block)
+    blockchain.add_transaction(sender=node_address, receiver='Suraj', amount=1)
     block = blockchain.create_block(proof, previous_hash)
     response = {
         'message': 'Congratulations!, you just mined a block!',
         'index': block['index'],
         'timestamp': block['timestamp'],
         'proof': block['proof'],
-        'previous_hash': block['previous_hash']
+        'previous_hash': block['previous_hash'],
+        'transactions': block['transactions']
     }
     return jsonify(response), 200
 
 
-# Getting the full blockchain
 @app.route('/get_chain', methods=['GET'])
 def get_chain():
     ''' Get current chain of block chain '''
@@ -37,7 +39,6 @@ def get_chain():
     return jsonify(response), 200
 
 
-# Checking if the blockchain is valid
 @app.route('/is_valid', methods=['GET'])
 def is_valid():
     ''' Checking blockchain is valid or not '''
@@ -50,7 +51,25 @@ def is_valid():
     return jsonify(response), 200
 
 
+@app.route('/add_transaction', methods=['POST'])
+def add_transaction():
+    ''' Adding a new transaction to the Blockchain '''
+    json = requests.get_json()
+    transaction_keys = ["sender", "reciever", "amount"]
+    if not all(key in json for key in transaction_keys):
+        return "Some elements of the transaction are missing", 400
+
+    index = blockchain.add_transaction(
+        json["sender"], json["reciever"], json["amount"])
+    response = {
+        'message': f'This transaction will be added to the Block {index}'}
+    return jsonify(response), 201
+
+
 if __name__ == '__main__':
+
+    # Creating an address for the node on Port 5000
+    node_address = str(uuid4()).replace("-", "")
 
     # Creating a Blockchain
     blockchain = Blockchain()
