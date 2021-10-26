20c20842f6b3091a00675ab171d60d4aea9928c0
surajkarki66 <suraj.karki500@protonmail.com>
2021-10-26 19:21:00 +0545

    Modified blockchain.py

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -90,21 +90,17 @@ class Blockchain:
         self.nodes.add(parsed_url.netloc)
 
     def replace_chain(self):
-        ''' Consensus method'''
         network = self.nodes
         longest_chain = None
         max_length = len(self.chain)
-
         for node in network:
             response = requests.get(f'http://{node}/get_chain')
             if response.status_code == 200:
-                length = response.json()["length"]
-                chain = response.json()["chain"]
-
+                length = response.json()['length']
+                chain = response.json()['chain']
                 if length > max_length and self.is_chain_valid(chain):
                     max_length = length
                     longest_chain = chain
-
         if longest_chain:
             self.chain = longest_chain
             return True
